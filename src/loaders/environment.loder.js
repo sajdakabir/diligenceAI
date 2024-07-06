ffb15d5211142118d73f581dd79bfe73877dcc89
sajdkabir <sajda.kbir@gmail.com>
2024-07-06 12:57:23 +0530

    feat: setup google env

--- src/loaders/environment.loder.js
+++ src/loaders/environment.loder.js
@@ -8,5 +8,10 @@ export const environment = {
     DB_NAME: process.env.DB_NAME,
     MONGO: process.env.MONGO,
     DB_HOST: process.env.DB_HOST,
-    PORT: process.env.PORT || 8080
+    PORT: process.env.PORT || 8080,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+    
 }
